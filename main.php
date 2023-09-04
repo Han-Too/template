aa16ec5cb2fe8f4b7ddfe90ae5858a1120ed950e
Han <68423335+Han-Too@users.noreply.github.com>
2023-09-04 11:32:01 +0700

    template

---
+++ main.php
@@ -0,0 +1,48 @@
+<main class="h-full overflow-y-auto">
+                <div class="container px-6 mx-auto grid">
+                    <h2 class="mt-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                        Dashboard
+                    </h2>
+                    <h4 class="mb-6 text-md font-medium text-gray-700 dark:text-gray-200">
+                        Data Dashboard
+                    </h4>
+
+                    <!-- New Table -->
+                    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+                        <div class="w-full mx-4 overflow-x-auto">
+                            <table class="w-full whitespace-no-wrap pb-4">
+                                <thead>
+                                    <tr
+                                        class="text-xs font-semibold tracking-wide text-center text-white uppercase border-b dark:border-gray-700 bg-purple-600 dark:text-white-400 dark:bg-purple-800">
+                                        <th class="px-4 py-3">User</th>
+                                        <th class="px-4 py-3">Telepon</th>
+                                        <th class="px-4 py-3">Status</th>
+                                        <th class="px-4 py-3">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                                    <tr class="text-gray-700 dark:text-gray-400 text-center">
+                                        <td class="px-4 py-3">
+                                            User
+                                        </td>
+                                        <td class="px-4 py-3 text-sm">
+                                            08888888
+                                        </td>
+                                        <td class="px-4 py-3 text-xs">
+                                            <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                                                Aktif
+                                            </span>
+                                        </td>
+                                        <td class="px-4 py-3 text-sm">
+                                            <div class="flex justify-center items-center">
+                                                <a href="#" class="px-2 py-1 mr-2 leading-tight text-dark bg-green-100 dark:bg-green-700 dark:text-green-100">Ubah</a>
+                                                <a href="#" class="px-2 py-1 ml-2 leading-tight text-dark bg-red-100 dark:bg-red-700 dark:text-red-100">Hapus</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
